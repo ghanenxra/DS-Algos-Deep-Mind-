690cdb64fce2168207115a26f4524327b6526dad
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-08-18 02:43:25 +0530

    Refactor searchRange method for clarity and efficiency

--- LeetCode/34. Find First and Last Position of Element in Sorted Array.py
+++ LeetCode/34. Find First and Last Position of Element in Sorted Array.py
@@ -1,30 +1,34 @@
 class Solution:
-    def searchRange(self, nums: list[int], target: int) -> list[int]:
-        def find_bound(is_first: bool) -> int:
-            left, right = 0, len(nums) - 1
-            bound = -1
-            
-            while left <= right:
-                mid = left + (right - left) // 2
-                
-                if nums[mid] == target:
-                    bound = mid
-                    if is_first:
-                        right = mid - 1
-                    else:
-                        
-                        left = mid + 1
-                elif nums[mid] > target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-                    
-            return bound
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(nums)-1
+        first = -1
 
-        start = find_bound(is_first=True)
-        # If the target isn't found in the first pass, it won't exist at all
-        if start == -1:
-            return [-1, -1]
-            
-        end = find_bound(is_first=False)
-        return [start, end]
+        while i<=j:
+            mid = (i+j)//2
+
+            if nums[mid] == target:
+                first = mid
+                j = mid-1
+            elif nums[mid]<target:
+                i = mid+1
+            elif nums[mid]>target:
+                j = mid-1
+
+        
+        i = 0
+        j = len(nums)-1
+        last = -1
+
+        while i<=j:
+            mid = (i+j)//2
+
+            if nums[mid] == target:
+                last = mid
+                i = mid+1
+            elif nums[mid]<target:
+                i = mid+1
+            elif nums[mid]>target:
+                j = mid-1
+
+        return [first, last]
