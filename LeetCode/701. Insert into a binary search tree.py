4c4fa98acc4d93c7b6fa641102c625883cd1f1ca
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-08-27 00:06:36 +0530

    Refactor insertIntoBST method in Solution class
    Refactor insertIntoBST method to improve readability and structure. Update comments and change variable names for clarity.

--- LeetCode/701. Insert into a binary search tree.py
+++ LeetCode/701. Insert into a binary search tree.py
@@ -1,57 +1,36 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
         new_node = TreeNode(val)
         if root is None:
             return new_node
-        current_node = root
-
-        while True:
-            if val < current_node.val:
-                if current_node.left is None:
-                    current_node.left = new_node
+        
+        temp = root
+        while temp:
+            if temp.val < val:
+                if temp.right is  None:
+                    temp.right = new_node
                     break
-                
-                current_node = current_node.left
+                temp = temp.right
             else:
-                if current_node.right is None:
-                    current_node.right = new_node
+                if temp.left is  None:
+                    temp.left = new_node
                     break
-                current_node = current_node.right
+                temp = temp.left
 
         return root
 
-
-
-
-
-
-        # if root.val <= val:
-        #     while current_node.left != None:
-        #         current_node = current_node.left
-        #     if current_node.val >= val:
-        #         if current_node.left is None:
-        #             current.left = new_node
-        #         else:
-        #             current.right = new_node
-        #     else: 
-        #         if current_node.right is None:
-        #             current_node.right = new_node
-
-        # if root.val >= val:           
-
-        #     while current_node.right != None:
-        #         current_node = current_node.right
-        #     if current_node.val>=val:
-        #         current_node.left = new_node
-        #     else:
-        #         current_node.right = new_node
-
-
-        # return root
-            
+        # if root.val > val:
+        #     return self.insertIntoBST(root.left,val)
+        # else:
+        #     return self.insertIntoBST(root.right, val)
