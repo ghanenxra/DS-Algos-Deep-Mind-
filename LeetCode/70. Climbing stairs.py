965363b16c0fd45988d17db1c7c2cee3cf8fb81a
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-09-06 22:32:52 +0530

    Optimize climbStairs with memoization
    Refactor climbStairs method to use memoization for optimization.

--- LeetCode/70. Climbing stairs.py
+++ LeetCode/70. Climbing stairs.py
@@ -1,13 +1,31 @@
+cache = {}
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        if n in cache:
+            return cache[n]
+        cache[n]=self.climbStairs(n-1)+self.climbStairs(n-2)
+        return cache[n]
+
+
+
         
-        prev2, prev1 = 1, 2
+
+
+
+
+
+        # if n <= 2:
+        #     return n
         
-        for _ in range(3, n + 1):
-            curr = prev1 + prev2
-            prev2 = prev1
-            prev1 = curr
+        # prev2, prev1 = 1, 2
         
-        return prev1
+        # for _ in range(3, n + 1):
+        #     curr = prev1 + prev2
+        #     prev2 = prev1
+        #     prev1 = curr
+        
+        # return prev1
