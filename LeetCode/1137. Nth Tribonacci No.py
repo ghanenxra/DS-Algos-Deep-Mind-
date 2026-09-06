5fd6ae9097592aba77b968e5b59958d0b898da78
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-09-06 15:50:31 +0530

    Add files via upload

---
+++ LeetCode/1137. Nth Tribonacci No.py
@@ -0,0 +1,8 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n==0 or n==1:
+            return n
+        if n==2:
+            return 1
+
+        return self.tribonacci(n-1)+self.tribonacci(n-2)+self.tribonacci(n-3)
